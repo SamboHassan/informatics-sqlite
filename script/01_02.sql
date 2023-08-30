9604aa09350e16ca12fec865ea542ce516262e56
Hassan Sambo <sambohasan@gmail.com>
2023-08-30 14:11:17 +0100

    sqlite update and delete operator

--- script/01_02.sql
+++ script/01_02.sql
@@ -1,1 +1,22 @@
 -- UPDATE OPERATION
+select *
+from persons;
+insert into persons
+values(6, 'test', 'kowai', 'yan leman');
+update persons
+set firstName = 'Brahaha',
+    lastName = 'Bizaar'
+where lastName = 'kowai';
+select *
+from persons;
+--########################
+update persons
+set address_ = "27 Katempe street"
+where firstName = "Tjasen"
+    and lastName = "Jakob";
+-- LIKE OPERATOR
+select firstName,
+    address_,
+    lastName
+from persons
+where firstName like "%sen%";
