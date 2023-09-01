fb62dd5297f5f55570994053141d98edbbcceb4f
Hassan Sambo <sambohasan@gmail.com>
2023-09-01 17:02:32 +0100

    add sqlite filter record lessons files

--- script/02_01.sql
+++ script/02_01.sql
@@ -1,36 +1,12 @@
 /*
  Created by: HASSAN SAMBO
  Date: 31-08-2023
- Description: Sql query to find out how many customers purchase two songs at 99 cents each?
+ Description: how many customers purchase two songs at 99 cents each?
  */
--- select InvoiceDate,
---     BillingAddress,
---     BillingCity,
---     Total
--- from Invoice
--- where Total = 1.98
--- order by InvoiceDate 
--- how many invoices exist between $1.98 and $5
--- select InvoiceDate,
---     BillingAddress,
---     BillingCity,
---     Total
--- from Invoice
--- where Total between 1.98 and 5
--- order by InvoiceDate 
--- how many invoices do we have that are exactly $1.98 or $3.96
--- select InvoiceDate,
---     BillingAddress,
---     BillingCity,
---     Total
--- from Invoice
--- where total in (1.98, 3.96)
--- order by InvoiceDate 
--- How many invoices where billed to the city of Brussels
 select InvoiceDate,
     BillingAddress,
     BillingCity,
     Total
 from Invoice
-where total in (1.98, 3.96)
+where Total = 1.98
 order by InvoiceDate
