e7d1becdf8dac6f914020fc8294994fc0bf1efb9
SamboHassan <sambohasan@gmail.com>
2024-11-04 21:00:12 +0100

    add files

--- script/02_07.sql
+++ script/02_07.sql
@@ -4,11 +4,12 @@
  Description: SQL Functions
  */
 SELECT c.FirstName || ' ' || c.LastName || ', ' || c.Address || ', ' || c.City || ', ' || c.PostalCode as 'Mailing address'
-from Customer c --#####################################################################################
-    -- NEW QUERY
+from Customer c;
+--#####################################################################################
+-- NEW QUERY
 SELECT length(PostalCode),
     SUBSTR(PostalCode, 1, 5) as 'postal code'
-FROM Customer
+FROM Customer;
 SELECT cus.PostalCode,
     substr(PostalCode, 1, 4) as 'subCode'
-FROM Customer cus
+FROM Customer cus;
